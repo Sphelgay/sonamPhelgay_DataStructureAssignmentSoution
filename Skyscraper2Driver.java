91b158a0a254fa963e58eee59ebc65a256429e00
Sphelgay <105509308+Sphelgay@users.noreply.github.com>
2022-08-01 22:53:17 +0600

    Add files via upload

---
+++ Skyscraper2Driver.java
@@ -0,0 +1,21 @@
+package io.test.fr;
+
+import java.util.Scanner;
+
+public class Skyscraper2Driver {
+	
+	public static void main(String[] args) {
+		System.out.println("Enter the total no of floors in the building : ");
+		try (Scanner sc = new Scanner(System.in)) {
+			int numberOfFloors = sc.nextInt();
+			int[] listOfFloors = new int[numberOfFloors];
+			for (int i = 0; i < numberOfFloors; i++) {
+				System.out.println("Enter the floor size given on day : " + (i + 1));
+				int floorSize = sc.nextInt();
+				listOfFloors[i] = floorSize;
+			}
+			Skyscraper.calculateFloors(listOfFloors);
+		}
+	}
+	
+}
